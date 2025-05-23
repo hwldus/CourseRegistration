bb653c0cb90a34aeeac6dbbc2b908097e64bdaff
JiYeon <89963270+hwldus@users.noreply.github.com>
2025-05-23 22:55:19 +0900

    feat : Swagger AP

--- CourseRegistration/src/main/java/com/assignment/CourseRegistration/controller/StudentController.java
+++ CourseRegistration/src/main/java/com/assignment/CourseRegistration/controller/StudentController.java
@@ -6,6 +6,8 @@ import com.assignment.CourseRegistration.dto.RegistrationResponseDTO;
 import com.assignment.CourseRegistration.dto.TutorResponseDTO;
 import com.assignment.CourseRegistration.model.AvailableTime;
 import com.assignment.CourseRegistration.service.StudentService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
@@ -17,11 +19,13 @@ import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/students")
+@Tag(name="학생 API", description = "학생의 수강신청 API")
 @RequiredArgsConstructor
 public class StudentController {
     private final StudentService studentService;
 
     @GetMapping("/availableTimes")
+    @Operation(summary = "수업 가능 시간대 조회", description = "기간 & 수업 길이로 현재 수업 가능한 시간대 조회")
     public ResponseEntity<List<AvailableTimeResponseDTO>> getAvailableTimes(@RequestParam("start") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime startTime,
                                                                             @RequestParam("end") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime endTime,
                                                                             @RequestParam("duration") int durationMinutes) {
@@ -33,6 +37,7 @@ public class StudentController {
     }
 
     @GetMapping("/availableTutors")
+    @Operation(summary = "수업 가능한 튜터 조회", description = "시간대 & 수업 길이로 수업 가능한 튜터 조회")
     public ResponseEntity<List<TutorResponseDTO>> getAvailableTutors(@RequestParam("start") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime startTime,
                                                                      @RequestParam("end") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime endTime,
                                                                      @RequestParam("duration") int durationMinutes) {
@@ -41,12 +46,14 @@ public class StudentController {
     }
 
     @PostMapping("/register")
+    @Operation(summary = "수강 신청", description = "시간대, 수업 길이, 튜터로 새로운 수업 신청")
     public ResponseEntity<String> createClass(@RequestBody RegistrationRequestDTO requestDTO) {
         studentService.createClass(requestDTO);
         return ResponseEntity.ok("수강신청되었습니다.");
     }
 
     @GetMapping("/classes")
+    @Operation(summary = "신청한 수업 조회", description = "학생이 신청 완료한 수업을 조회")
     public ResponseEntity<List<RegistrationResponseDTO>> getRegisteredClasses(@RequestParam Long studentId) {
         return ResponseEntity.ok(studentService.getRegistration(studentId));
     }
--- CourseRegistration/src/main/java/com/assignment/CourseRegistration/controller/TutorController.java
+++ CourseRegistration/src/main/java/com/assignment/CourseRegistration/controller/TutorController.java
@@ -2,22 +2,27 @@ package com.assignment.CourseRegistration.controller;
 
 import com.assignment.CourseRegistration.dto.AvailableTimeDTO;
 import com.assignment.CourseRegistration.service.TutorService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/tutors")
+@Tag(name="튜터 API", description = "튜터의 시간 API")
 @RequiredArgsConstructor
 public class TutorController {
     private final TutorService tutorService;
     @PostMapping("/{tutorId}/availableTime")
+    @Operation(summary = "수업 생성", description = "튜터가 수업 가능한 시간 생성(30분 또는 60분)")
     public ResponseEntity<String> createAvailableTime(@PathVariable Long tutorId,
                                                       @RequestBody AvailableTimeDTO request) {
         tutorService.createAvailableTime(tutorId, request.getStartTime(), request.getDurationMinutes());
         return ResponseEntity.ok("수업 가능한 시간이 생성되었습니다.");
     }
     @DeleteMapping("/{tutorId}/availableTime/{availableTimeId}")
+    @Operation(summary = "수업 삭제", description = "튜터가 수업 삭제")
     public ResponseEntity<String> deleteAvailableTime(@PathVariable Long tutorId,
                                                       @PathVariable Long availableTimeId) {
         tutorService.deleteAvailableTime(tutorId, availableTimeId);
